30d20be06b06b91c2c45a46f09abfbda1ce7aa7a
Ahmed <a.alkaff@tiraforit.com>
2021-04-03 21:27:14 +0300

    view binding, Intent

---
+++ app/src/main/java/com/pioneersacademy/alkaff/firstapplication/Keys.kt
@@ -0,0 +1,8 @@
+package com.pioneersacademy.alkaff.firstapplication
+
+class Keys {
+    companion object {
+        val Name:String = "name"
+        val Password:String = "password"
+    }
+}
--- app/src/main/java/com/pioneersacademy/alkaff/firstapplication/MainActivity.kt
+++ app/src/main/java/com/pioneersacademy/alkaff/firstapplication/MainActivity.kt
@@ -2,10 +2,22 @@ package com.pioneersacademy.alkaff.firstapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 
+// read more about intent at https://developer.android.com/guide/components/intents-filters
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        //
+        val name = intent.getStringExtra(Keys.Name)
+        // Make sure that the key value pair is sent with the intent
+        if(name != null)
+            Log.d("MainActivity","${name}")
+
+        // You can skip the null check because we have a default value
+        Log.d("MainActivity","${intent.getIntExtra(Keys.Password,0)}")
+
+
     }
 }
